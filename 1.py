bc153fe3c6ae508b543477baf7edd57807d86042
drazioti <drazioti@csd.auth.gr>
2022-07-14 18:27:02 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 ''' code for solving the diophantine equation : y^2 = (x+a)(x+a+k)(x+b)(x+b+k)
+    The function takes three inputs H(a,b,k).
     sage:H(1,2,41)
     [[7, 420], [-51, 420], [-22, 420], [-1, 0], [-43, 0], [-2, 0], [-42, 0]]
     
