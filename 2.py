992cfe827307156e59fca6dc821c232a246c13a7
drazioti <drazioti@csd.auth.gr>
2022-07-14 23:15:38 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,7 @@
 ''' code for solving the diophantine equation : y^2 = c^2x^4+ax^2+b
     The function takes three inputs H2(c,a,b) and returns the integer solutions with y>=0.
     sage:H2(3,7,-8)
-    [[7, 420], [-51, 420], [-22, 420], [-1, 0], [-43, 0], [-2, 0], [-42, 0]]
+    [[3, 84], [-3, 84]]
 '''
 
 def H2(c,a,b):
@@ -16,8 +16,8 @@ def H2(c,a,b):
                 sqr_delta = int(sqrt(Delta))
                 x1,x2 = sqr_delta,-sqr_delta
                 y = abs((d1-d2)/4)
-                L.append([x1,y,d1,d2])
-                if [x2,y,d1,d2] not in L:
-                    L.append([x2,y,d1,d2])
+                L.append([x1,y])
+                if [x2,y] not in L:
+                    L.append([x2,y])
     print(L)
     return 
