6c23d8c57c7ee8309e0422b0e9e7e2915eed4776
drazioti <drazioti@csd.auth.gr>
2022-07-17 17:10:18 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -4,10 +4,20 @@
     [[3, 28], [-3, 28]]
 '''
 
-def H2(c,a,b):
+''' code for solving the diophantine equation : y^2 = c^2x^4+ax^2+b
+    The function takes three inputs H2(c,a,b) and returns the integer solutions with y>=0.
+    sage:H2(3,7,-8)
+    [[3, 28], [-3, 28]]
+    sage:H2(11,21,2)
+    [[1, 12], [-1, 12]]
+'''
+
+def H2git(c,a,b):
     L = []
     n = a^2-4*b*c^2
-    DIV = divisors(n)
+    DIV_positive = divisors(n)
+    DIV_minus = [-O for O in DIV_positive]
+    DIV = DIV_positive+DIV_minus
     for d1 in DIV:
         d2 = n/d1
         if mod((d1-d2),2)==0 and d1<=d2:
@@ -21,3 +31,4 @@ def H2(c,a,b):
                     L.append([x2,y])
     print(L)
     return 
+
